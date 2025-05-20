af7e94a0b8832d3cc6300bcf9b0d760522168264
WithFortuna <rmsghchl0@naver.com>
2025-05-20 15:43:58 +0900

    [Init] springInitializer setup

---
+++ src/main/java/com/ceos/spring_vote_21st/CeosBeSpringVote21stApplication.java
@@ -0,0 +1,13 @@
+package com.ceos.spring_vote_21st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeosBeSpringVote21stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeosBeSpringVote21stApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceos/spring_vote_21st/CeosBeSpringVote21stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos.spring_vote_21st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeosBeSpringVote21stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
