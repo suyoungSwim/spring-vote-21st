4b23bd23699eed804ce7cd9aa53d69cd809190de
LEE <sw09112@yonsei.ac.kr>
2025-05-23 15:57:41 +0900

    [CHORE] Dockerfile, docker-compose.yaml, health check controller

---
+++ src/main/java/com/ceos/spring_vote_21st/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.ceos.spring_vote_21st.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
