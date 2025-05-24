f54b27fd145d23effecad925e4e2c89fbb45423b
GNO CHOI <rmsghchl0@naver.com>
2025-05-24 15:01:30 +0900

    [FEAT] Config 빈 등록
    기존에 빈으로 등록하지 않았던 SecurityConfig를 빈으로 등록

--- src/main/java/com/ceos/spring_vote_21st/auth/config/SecurityConfig.java
+++ src/main/java/com/ceos/spring_vote_21st/auth/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.ceos.spring_vote_21st.auth.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean
