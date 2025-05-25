d1c236e816d23a9d366b5e2084a5d3a301dfbc38
GNO CHOI <rmsghchl0@naver.com>
2025-05-25 14:18:47 +0900

    [FEAT] 선거 기간 필드 추가

--- src/main/java/com/ceos/spring_vote_21st/CeosBeSpringVote21stApplication.java
+++ src/main/java/com/ceos/spring_vote_21st/CeosBeSpringVote21stApplication.java
@@ -1,8 +1,13 @@
 package com.ceos.spring_vote_21st;
 
+import com.ceos.spring_vote_21st.security.auth.application.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class CeosBeSpringVote21stApplication {
 
