cfc4e85b1a23ce0d6a3e46d2d01678ab443a96a4
GNO CHOI <rmsghchl0@naver.com>
2025-05-25 14:18:01 +0900

    [FEAT] 블랙리스트 기능 및 로그인 기능 추가

---
+++ src/main/java/com/ceos/spring_vote_21st/security/auth/web/dto/ReissueResponseDTO.java
@@ -0,0 +1,9 @@
+package com.ceos.spring_vote_21st.security.auth.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public class ReissueResponseDTO {
+    private String accessToken;
+}
