9d0852e883ec7f4fde81c87d4d1ce731e6c605cc
GNO CHOI <rmsghchl0@naver.com>
2025-05-25 14:16:18 +0900

    [FEAT] 회원 엔티티 수정
     - 권한 필드 추가
     - username기반 조회 함수 추가

--- src/main/java/com/ceos/spring_vote_21st/member/repository/MemberRepository.java
+++ src/main/java/com/ceos/spring_vote_21st/member/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package com.ceos.spring_vote_21st.member.repository;
 import com.ceos.spring_vote_21st.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    Optional<Member> findByUsername(String username);
 }
