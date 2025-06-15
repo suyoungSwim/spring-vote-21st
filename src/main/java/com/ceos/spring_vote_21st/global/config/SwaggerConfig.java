59cf3ab9868abe7ecdaf7a29cb7c3ef16d27004e
WithFortuna <rmsghchl0@naver.com>
2025-06-16 01:10:48 +0900

    [FEAT] Swagger 인증기능 추가
    스웨거에서 인증이 가능하도록 추가한다

--- src/main/java/com/ceos/spring_vote_21st/global/config/SwaggerConfig.java
+++ src/main/java/com/ceos/spring_vote_21st/global/config/SwaggerConfig.java
@@ -11,6 +11,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .info(new Info().title("API 문서").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("AccessToken( Bearer없이 토큰만 넣어주세요:) )"))
+                .components(new Components()
+                        .addSecuritySchemes("AccessToken( Bearer없이 토큰만 넣어주세요:) )", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT")))
+
+                .paths(new Paths().addPathItem("/api/v1/auth/signin", loginPathItem))
                 // Swagger-UI Try it out → "/api/..." 상대 경로로 호출
                 .addServersItem(new Server().url("/"));
     }
