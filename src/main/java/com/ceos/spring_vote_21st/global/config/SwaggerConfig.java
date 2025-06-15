f727e66247c05039596c06d722bb6eb6d582c328
WithFortuna <rmsghchl0@naver.com>
2025-06-16 01:12:51 +0900

    [FIX] 로그아웃 필터 반영
    - 액세스토큰이 로그아웃된 토큰인지 검증하지 않았던 오류 해결

--- src/main/java/com/ceos/spring_vote_21st/global/config/SwaggerConfig.java
+++ src/main/java/com/ceos/spring_vote_21st/global/config/SwaggerConfig.java
@@ -1,6 +1,13 @@
 package com.ceos.spring_vote_21st.global.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
