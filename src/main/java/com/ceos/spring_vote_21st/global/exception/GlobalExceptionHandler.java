05ac63f6e844edcb03b6c5a02a8b3436c3c8d38b
WithFortuna <rmsghchl0@naver.com>
2025-06-20 09:28:02 +0900

    [FEAT] GlobalExceptionHandler 스웨거 제외

--- src/main/java/com/ceos/spring_vote_21st/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ceos/spring_vote_21st/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ceos.spring_vote_21st.global.exception;
 
 import com.ceos.spring_vote_21st.global.response.domain.ServiceCode;
 import com.ceos.spring_vote_21st.global.response.dto.CommonResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<CommonResponse> handleCustomException(CustomException e) {
+    public ResponseEntity<CommonResponse> handleCustomException(CustomException e, HttpServletRequest request) {
+        if (request.getRequestURI().startsWith("/v3/api-docs") || request.getRequestURI().contains("swagger")) {
+            // Swagger 문서 생성용 요청은 예외 처리하지 않음
+            return ResponseEntity.ok().build();
+        }
+
         log.info("globalExceptionHandler에 진입" + e.getMessage());
         ServiceCode serviceCode = e.getServiceCode();
         log.error("CustomException 발생: 상태코드={}, 메시지={}, 에러명={}",
